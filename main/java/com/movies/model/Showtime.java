0686248018a5cd517bded000cf251526d6bdcc8e
Gowtham12448 <70027172+Gowtham12448@users.noreply.github.com>
2026-09-13 22:43:05 +0530

    Add Showtime class with time and price attributes

--- main/java/com/movies/model/Showtime.java
+++ main/java/com/movies/model/Showtime.java
@@ -1,1 +1,14 @@
+package com.movies.model;
 
+public class Showtime {
+    private String time;
+    private double price;
+
+    public Showtime(String time, double price) {
+        this.time = time;
+        this.price = price;
+    }
+
+    public String getTime() { return time; }
+    public double getPrice() { return price; }
+}
