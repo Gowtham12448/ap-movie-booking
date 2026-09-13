44324c990d912c0db44cc67b5c5a732d7e21e4aa
Gowtham12448 <70027172+Gowtham12448@users.noreply.github.com>
2026-09-13 22:40:21 +0530

    Create Movie class with attributes and constructor
    Added Movie class with properties and constructor.

--- main/java/com/movies/model/Movie.java
+++ main/java/com/movies/model/Movie.java
@@ -1,1 +1,30 @@
+package com.movies.model;
 
+public class Movie {
+    private int id;
+    private String title;
+    private String language;
+    private String genre;
+    private String duration;
+    private String rating;
+    private String poster;
+
+    public Movie(int id, String title, String language, String genre,
+                 String duration, String rating, String poster) {
+        this.id = id;
+        this.title = title;
+        this.language = language;
+        this.genre = genre;
+        this.duration = duration;
+        this.rating = rating;
+        this.poster = poster;
+    }
+
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public String getLanguage() { return language; }
+    public String getGenre() { return genre; }
+    public String getDuration() { return duration; }
+    public String getRating() { return rating; }
+    public String getPoster() { return poster; }
+}
