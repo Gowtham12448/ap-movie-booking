cbe369ee0b9544065cc8334c17b45b1723bc5262
Gowtham12448 <70027172+Gowtham12448@users.noreply.github.com>
2026-09-13 22:45:40 +0530

    Implement HomeServlet to handle GET requests

--- main/java/com/movies/servlet/HomeServlet.java
+++ main/java/com/movies/servlet/HomeServlet.java
@@ -1,1 +1,19 @@
+package com.movies.servlet;
 
+import com.movies.data.DataStore;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute("movies", DataStore.MOVIES);
+        req.setAttribute("locations", DataStore.allLocations());
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+}
