6c512c548c6ffe447117bea3fa99593fc622e491
Gowtham12448 <70027172+Gowtham12448@users.noreply.github.com>
2026-09-13 22:40:46 +0530

    Add Theater class with properties and constructor

--- main/java/com/movies/model/Theater.java
+++ main/java/com/movies/model/Theater.java
@@ -1,1 +1,20 @@
+package com.movies.model;
 
+public class Theater {
+    private int id;
+    private String name;
+    private String location;
+    private String address;
+
+    public Theater(int id, String name, String location, String address) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.address = address;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getLocation() { return location; }
+    public String getAddress() { return address; }
+}
